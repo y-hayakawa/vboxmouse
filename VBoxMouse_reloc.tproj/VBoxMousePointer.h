c7be75ad4c665c4d2889f583958e40710e3fd8cc
Yoshinori Hayakawa <yoshinori.hayakawa@me.com>
2025-06-20 12:22:56 +0900

    Delete VBoxMouse_reloc.tproj directory

--- VBoxMouse_reloc.tproj/VBoxMousePointer.h
+++
@@ -1,9 +0,0 @@
-#define	DRIVER_PRIVATE
-#import "EventSrcPCPointer.h"
-#undef DRIVER_PRIVATE
-
-#import "VBoxDefines.h"
-
-@interface EventSrcPCPointer(VBoxMousePointer)
-- processVBoxMouseInput:(struct vbox_mouse_absolute_ex *)input;
-@end
